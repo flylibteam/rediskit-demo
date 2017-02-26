30b69301604bfea8c2cbc7a36a28bb8f3e3c3694
frank_liu_1 <liushaomingdev@163.com>
2017-02-26 22:41:13 +0800

    Init

---
+++ src/main/java/net/flylib/redisdemo/App.java
@@ -0,0 +1,21 @@
+package net.flylib.redisdemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import net.flylib.rediskit.RedisClient;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("appContext.xml");
+		RedisClient redisClient = appContext.getBean("redisClient", RedisClient.class);
+		test(redisClient);
+		System.out.println("End.");
+	}
+	
+	private static void test(RedisClient client) {
+    	String k = "key1";
+    	client.set(k, "aaa");
+    	System.out.println("value of " + k + " is: "  + client.get(k));
+	}
+}
